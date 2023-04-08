6adcf1f12df376ce51e003d3137841d85cf5a729
herlmanoel <herlmanoelfernandes@gmail.com>
2023-04-08 15:05:25 -0300

    feat: reescrevendo components

--- components/Projects/components/Card/index.tsx
+++ components/Projects/components/Card/index.tsx
@@ -5,9 +5,9 @@ import { ProjectType } from '../../data/projects';
 export const Card = ({ id, titulo, descricao, tecnologias, img, link  }: ProjectType) => {
     descricao = descricao.substring(0, 134) + "...";
     return (
-        <div key={id} className="card-div my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <article className="card-project-article overflow-hidden rounded-lg shadow-lg">
-                <div className="card-project-body">
+        <div key={id} className="card-div my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 ">
+            <article className="card-project-article overflow-hidden rounded-lg shadow-lg h-full">
+                <div className="card-project-body flex flex-col justify-between">
                     <div className="img-card overflow-hidden">
                         <Image objectFit="cover" className="block h-auto w-full" src={img} alt={descricao} />
                     </div>
@@ -27,7 +27,7 @@ export const Card = ({ id, titulo, descricao, tecnologias, img, link  }: Project
                             </p>
                         </a>
                     </main>
-                    <footer className="px-6 pt-4 pb-2">
+                    <footer className="flex justify-start items-center px-6 pt-4 pb-2">
                         <div>
                             {tecnologias.map(tec => {
                                 return (<span key={8} className="inline-block bg-teal-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-100 mr-2 mb-2 cursor-pointer hover:bg-teal-600 transition delay-50 duration-300 ease-in-out">{tec}</span>)
